d28465625d7f697c175e4c654030ee747328bee7
TaraniukD <taranyk88@ukr.net>
2022-12-10 20:06:50 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import './css/styles.css';
 import { fetchCountries } from './fetchCountries';
 import { warningInfo, warningError } from './warningInfo';
 import { createCountryList, createCountryInfo } from './createListElement';
-import 'notiflix/dist/notiflix-3.2.5.min.css';
 import debounce from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
